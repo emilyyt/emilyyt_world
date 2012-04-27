17e7c5bdcade9481c4cd5a24388a8cb8a599e505
Julia Teitelbaum <juliat@cmu.edu>
2012-04-26 20:12:57 -0400

    set up relationship between article and category models

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,9 @@ class Article < ActiveRecord::Base
 	# Validations
 	validates_presence_of :title, :content
 	
+	# Relationships
+	belongs_to :category
+	
 	# Scopes
 	scope :alphabetical, order('title')
 	scope :active, where('active = ?', true)
